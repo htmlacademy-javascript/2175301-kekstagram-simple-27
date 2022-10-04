3d5737b65c365e1006ae67cd1b6c12be22cc60e4
Александр <alexandr.kamkov2012@gmail.com>
2022-10-05 00:16:26 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Источник mdn. Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min); //минимальное значение округленное вверх до целого
-  max = Math.floor(max); //максимальное значение округленное вниз до целого
-  if (max <= min || min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
+  const minRound = Math.ceil(min); //минимальное значение округленное вверх до целого
+  const maxRound = Math.floor(max); //максимальное значение округленное вниз до целого
+  if (maxRound <= minRound || minRound < 0 || maxRound < 0 || typeof minRound !== 'number' || typeof maxRound !== 'number') {
     return NaN;
-  } else {return Math.floor(Math.random() * ((max - min + 1)) + min) * 100;} //вернет целое число округленное вниз (Максимум и минимум включаются).
+  } else {return Math.floor(Math.random() * ((maxRound - minRound + 1)) + minRound);} //вернет целое число округленное вниз (Максимум и минимум включаются).
 }
-getRandomIntInclusive (0.5,1.5);
+getRandomIntInclusive (0,2.5);
 
 //Функция для проверки максимальной длинны строки
 
