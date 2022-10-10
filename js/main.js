30b36a86b6bd77f421c3fef1a8ab61d74f781ecb
Александр <alexandr.kamkov2012@gmail.com>
2022-10-10 09:38:51 +0300

    “”

--- js/main.js
+++ js/main.js
@@ -24,11 +24,7 @@ checkMaxStringLength ('',50);
 
 // Создаем массивы с элементами/значениям объектов
 
-const ID = [Array.from({length: 25}, (_, i) => i + 1)];
-const URL = ['photos/1.jpg','photos/2.jpg','photos/3.jpg','photos/4.jpg','photos/5.jpg','photos/6.jpg','photos/7.jpg','photos/8.jpg','photos/9.jpg','photos/10.jpg','photos/11.jpg','photos/12.jpg','photos/13.jpg','photos/14.jpg','photos/15.jpg','photos/16.jpg','photos/17.jpg','photos/18.jpg','photos/19.jpg','photos/20.jpg','photos/21.jpg','photos/22.jpg','photos/23.jpg','photos/24.jpg','photos/25.jpg'];
-const DESCRIPTION = ['авангардистский','авангардный','аванпостный','авансовый','авантажный','авантюриновый','авантюристический','авантюристский','авантюрный','аварийный','аварский','августейший','августовский','авиадесантный','авиамодельный','авиамоторный','авианосный','авиапочтовый','авиаремонтный','авиаспортивный','авиастроительный','авиаторский','авиатранспортный','авиационный','авитаминозный'];
-const LIKES = [Array.from({length: 186}, (_, i) => i + 15)];
-const COMMETS = [Array.from(Array(201).keys())];
+
 
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
@@ -40,23 +36,33 @@ const getRandomIntInclusive = (min, max) => {
   } else {return Math.floor(Math.random() * ((maxRound - minRound + 1)) + minRound);}
 };
 
-// Функция, возвращающая случайный элемент, может ли он повторитья - да, как сделать иначе, через условие?
+// Функция, возвращающая случайный элемент, НЕ УВЕРЕН В ПРАВИЛЬНОСТИ ЭТОГО БЛОКА
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomIntInclusive(0,elements.length-1)]
+const indexes = [];
+const getRandomArrayElement = (indexes) => {
+const index = getRandomIntInclusive(1, 25);
+indexes.forEach((index) => {
+  return (indexes.includes(index)) ? getRandomArrayElement() : indexes.push(index);
+})
 };
 
-// Функция, создания объекта
+const ID = Array.from({length: 25}, (_, i) => i + 1);
+const URL = ['photos/1.jpg','photos/2.jpg','photos/3.jpg','photos/4.jpg','photos/5.jpg','photos/6.jpg','photos/7.jpg','photos/8.jpg','photos/9.jpg','photos/10.jpg','photos/11.jpg','photos/12.jpg','photos/13.jpg','photos/14.jpg','photos/15.jpg','photos/16.jpg','photos/17.jpg','photos/18.jpg','photos/19.jpg','photos/20.jpg','photos/21.jpg','photos/22.jpg','photos/23.jpg','photos/24.jpg','photos/25.jpg'];
+const DESCRIPTION = ['авангардистский','авангардный','аванпостный','авансовый','авантажный','авантюриновый','авантюристический','авантюристский','авантюрный','аварийный','аварский','августейший','августовский','авиадесантный','авиамодельный','авиамоторный','авианосный','авиапочтовый','авиаремонтный','авиаспортивный','авиастроительный','авиаторский','авиатранспортный','авиационный','авитаминозный'];
+const LIKES = Array.from({length: 186}, (_, i) => i + 15);
+const COMMETS = Array.from(Array(201).keys());
 
-const createDescriptionFoto = () => {
-return {
-id: getRandomArrayElement(ID),
-url: getRandomArrayElement(URL),
-description: getRandomArrayElement(DESCRIPTION),
-likes: getRandomArrayElement(LIKES),
-commets: getRandomArrayElement(COMMETS)
+// Функция, создания объекта НЕ УВЕРЕН В ПРАВИЛЬНОСТИ и ЭТОГО БЛОКА
+
+function createDescriptionFoto() {
+  return {
+    id: getRandomArrayElement(1,25),
+    url: getRandomArrayElement(1,25),
+    description: getRandomArrayElement[DESCRIPTION(0, DESCRIPTION.length-1)],
+    likes: getRandomIntInclusive(15,200),
+    commets: getRandomIntInclusive(0,200)
+  };
 }
-};
 
 // Функция, генерирующая несколько объектов
 
