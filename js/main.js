eeff646a379e5bd5ffd20aa6ac8743c8ba7c8eab
Александр <alexandr.kamkov2012@gmail.com>
2022-10-11 17:52:03 +0300

    “complite”

--- js/main.js
+++ js/main.js
@@ -22,11 +22,7 @@ function checkMaxStringLength(verifiedString, maxLength) {
   }
 }
 checkMaxStringLength ('',50);
-
-// Создаем массивы с элементами/значениям объектов
-
 */
-
 
 //const indexes = [];
 //const getRandomArrayElement = () => {
@@ -34,12 +30,11 @@ checkMaxStringLength ('',50);
 //return (indexes.includes(index)) ? getRandomArrayElement() : indexes.push(index);
 //};
 
-//версия 2
-const ID = Array.from({length: 25}, (_, i) => i + 1);
-const URL = ['photos/1.jpg','photos/2.jpg','photos/3.jpg','photos/4.jpg','photos/5.jpg','photos/6.jpg','photos/7.jpg','photos/8.jpg','photos/9.jpg','photos/10.jpg','photos/11.jpg','photos/12.jpg','photos/13.jpg','photos/14.jpg','photos/15.jpg','photos/16.jpg','photos/17.jpg','photos/18.jpg','photos/19.jpg','photos/20.jpg','photos/21.jpg','photos/22.jpg','photos/23.jpg','photos/24.jpg','photos/25.jpg'];
-const DESCRIPTION = ['авангардистский', 'авангардный', 'аванпостный', 'авансовый', 'авантажный', 'авантюриновый', 'авантюристический', 'авантюристский', 'авантюрный', 'аварийный', 'аварский', 'августейший', 'августовский', 'авиадесантный', 'авиамодельный', 'авиамоторный', 'авианосный', 'авиапочтовый', 'авиаремонтный', 'авиаспортивный', 'авиастроительный', 'авиаторский', 'авиатранспортный', 'авиационный', 'авитаминозный'];
-const LIKES = Array.from({length: 186}, (_, i) => i + 15);
-const COMMETS = Array.from(Array(201).keys());
+//Урок 4.15
+
+const descriptions = ['авангардистский', 'авангардный', 'аванпостный', 'авансовый', 'авантажный', 'авантюриновый', 'авантюристический', 'авантюристский', 'авантюрный', 'аварийный', 'аварский', 'августейший', 'августовский', 'авиадесантный', 'авиамодельный', 'авиамоторный', 'авианосный', 'авиапочтовый', 'авиаремонтный', 'авиаспортивный', 'авиастроительный', 'авиаторский', 'авиатранспортный', 'авиационный', 'авитаминозный'];
+const like = Array.from({length: 186}, (_, i) => i + 15);
+const commets = Array.from({length: 201}, (_, i) => i);
 
 const COUNT_OBJ = 25;
 
@@ -50,18 +45,19 @@ const getRandomIntInclusive = (min, max) => {
     return NaN;
   } else { return Math.floor(Math.random() * ((maxRound - minRound + 1)) + minRound); }
 };
+getRandomIntInclusive();
 
 const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
 
-const createDescriptionFoto = () => ({
-  id: getRandomArrayElement(ID),
-  url: `photos${getRandomArrayElement(URL)}.jpg`,
-  description: DESCRIPTION[(getRandomIntInclusive(0, DESCRIPTION.length - 1))],
-  likes: getRandomArrayElement(LIKES),
-  commets: getRandomArrayElement(COMMETS),
+const createDescriptionFoto = (id) => ({
+  id: id,
+  url: `photos${id}.jpg`,
+  description: descriptions[(getRandomIntInclusive(0, descriptions.length - 1))],
+  likes: getRandomArrayElement(like),
+  commets: getRandomArrayElement(commets),
 });
 
-const createSumFoto = Array.from({length: COUNT_OBJ}, createDescriptionFoto);
+const createSumFoto = Array.from({length: COUNT_OBJ}, (_element, index) => createDescriptionFoto(index + 1));
 
 createSumFoto();
 
