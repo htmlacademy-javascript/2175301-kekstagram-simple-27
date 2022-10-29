8a700dc3606b29e25d6559207eb3e31bc3fb61b6
Александр <alexandr.kamkov2012@gmail.com>
2022-10-29 10:40:57 +0300

    Правда или действие 1

--- js/form.js
+++ js/form.js
@@ -4,37 +4,50 @@ const userModalElement = document.querySelector('.img-upload__overlay');
 
 const userOpenModalElement = document.querySelector('#upload-file');
 
-const userCloseModalElement = document.querySelector('#upload-cansel');
+const userCloseModalElement = document.querySelector('#upload-cancel');
 
 const modalElement = document.querySelector('body');
 
-const onModalEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeUserModal();
-  }
-};
+const formSubmit = document.querySelector('.img-upload__form');
 
-function openUserModal () {
+const openUserModal = () => {
   userModalElement.classList.remove('hidden');
   modalElement.classList.add('modal-open');
 
   document.addEventListener('keydown', onModalEscKeydown);
-}
+};
 
-function closeUserModal () {
+const closeUserModal = () => {
+  formSubmit.reset();
   userModalElement.classList.add('hidden');
   modalElement.classList.remove('modal-open');
-
   document.removeEventListener('keydown', onModalEscKeydown);
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  closeUserModal();
+};
+
+function onModalEscKeydown (evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUserModal();
+  }
 }
 
-userOpenModalElement.addEventListener('click', () => {
+userOpenModalElement.addEventListener('change', () => {
   openUserModal();
 });
 
 userCloseModalElement.addEventListener('click', () => {
   closeUserModal();
 });
+
+userCloseModalElement.removeEventListener('click', () => {
+  closeUserModal();
+});
+
+formSubmit.addEventListener('submit',onFormSubmit);
 
 export {openUserModal,closeUserModal};
