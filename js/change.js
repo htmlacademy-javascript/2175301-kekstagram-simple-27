cd7f36f363f9d0d60cedf91fc853871caeba358d
Александр <alexandr.kamkov2012@gmail.com>
2022-10-30 19:04:01 +0300

    посмотри пожалуйста

---
+++ js/change.js
@@ -0,0 +1,44 @@
+
+const fieldsShange = document.querySelector('.scale__control--value');
+
+const buttonSmaller = document.querySelector('.scale__control--smaller');
+
+const buttonBigger = document.querySelector('.scale__control--bigger');
+
+const fragmentPictures = document.querySelector('.img-upload__preview');
+
+
+const setScale = (scale) => {
+  fieldsShange.value = `${scale}%`;
+  fragmentPictures.style.transform = `scale(${scale / 100})`;
+};
+
+buttonBigger.addEventListener('click', () => {
+  const scale = parseInt(fieldsShange.value, 10);
+  if (scale < 100 && scale >= 25) {
+    setScale(scale + 25);
+  }
+});
+
+buttonSmaller.addEventListener('click', () => {
+  const scale = parseInt(fieldsShange.value, 10);
+  if (scale <= 100 && scale > 25) {
+    setScale(scale - 25);
+  }
+});
+
+
+/*
+noUiSlider.create(field, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 25,
+});
+
+field.noUiSlider.on('update', () => {
+  fieldsShange.value = field.noUiSlider.get();
+});
+*/
