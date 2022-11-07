2f7aafc39c195691e8eb1d46f5d3c0c5529546ef
Александр <alexandr.kamkov2012@gmail.com>
2022-11-07 20:09:40 +0300

    Помощь друга

--- js/change-img.js
+++ js/change-img.js
@@ -1,24 +1,23 @@
-
-const fieldsShange = document.querySelector('.scale__control--value');
-
-const buttonSmaller = document.querySelector('.scale__control--smaller');
-
-const buttonBigger = document.querySelector('.scale__control--bigger');
-
-const fragmentPictures = document.querySelector('.img-upload__preview img');
-
 const SCALE = 100;
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const STEP_SCALE = 25;
 
+const fieldsShangeElement = document.querySelector('.scale__control--value');
+
+const buttonSmallerElement = document.querySelector('.scale__control--smaller');
+
+const buttonBiggerElement = document.querySelector('.scale__control--bigger');
+
+const fragmentPicturesElement = document.querySelector('.img-upload__preview img');
+
 const setScale = (value = SCALE) => {
-  fragmentPictures.style.transform = `scale(${value / 100})`;
-  fieldsShange.value = `${value}%`;
+  fragmentPicturesElement.style.transform = `scale(${value / 100})`;
+  fieldsShangeElement.value = `${value}%`;
 };
 
 const buttonBiggerClick = () => {
-  const currentValue = parseInt(fieldsShange.value, 10);
+  const currentValue = parseInt(fieldsShangeElement.value, 10);
   let newValue = currentValue + STEP_SCALE;
   if (newValue > MAX_SCALE) {
     newValue = MAX_SCALE;
@@ -27,7 +26,7 @@ const buttonBiggerClick = () => {
 };
 
 const buttonSmallerClick = () => {
-  const currentValue = parseInt(fieldsShange.value, 10);
+  const currentValue = parseInt(fieldsShangeElement.value, 10);
   let newValue = currentValue - STEP_SCALE;
   if ( newValue < MIN_SCALE) {
     newValue = MIN_SCALE;
@@ -39,7 +38,7 @@ const scaleReset = () => {
   setScale();
 };
 
-buttonBigger.addEventListener('click', buttonBiggerClick);
-buttonSmaller.addEventListener('click', buttonSmallerClick);
+buttonBiggerElement.addEventListener('click', buttonBiggerClick);
+buttonSmallerElement.addEventListener('click', buttonSmallerClick);
 
 export { scaleReset };
